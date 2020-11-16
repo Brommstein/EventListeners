630e3c4c1554353cf9aa56303a9dd469a7c267e2
Ryan Conley <ryandeanconley2@yahoo.com>
2020-11-16 16:09:13 -0600

    completion

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 /* eslint-disable strict */
-function main(){
-  
+function main() {
+  //prevent default
+  $('#js-shopping-list-form').submit(event => {
+    event.preventDefault();
+
+    const list=($(event.currentTarget).find('input[name="shopping-list-entry"]').val());
+    $('ul').append(`<li><span class="shopping-item"> ${list} </span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button>
+    <button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li></ul>`);
+    console.log(list);
+  });
 }
+$('ul').on('click', '.shopping-item-toggle', function(event){
+  $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+});
+
+$('ul').on('click', '.shopping-item-delete', function(event){
+  $(this).closest('li').remove();
+});
 
 // eslint-disable-next-line no-undef
 $(main);
