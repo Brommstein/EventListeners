26e3bc32f1fe19ef5b785d9d950fbbf1bb426ca1
Ryan Conley <ryandeanconley2@yahoo.com>
2020-11-16 12:22:21 -0600

    correction to start

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 /* eslint-disable strict */
 function main(){
-  let shoppingList=[];
-
-  $('.add').click(function(){
-    return shoppingList.push('.item');
-  });
-
-  console.log(shoppingList);
-
+  
 }
 
 // eslint-disable-next-line no-undef
