12987935b955c76ddfdae6a1439c82e8f308bf30
Ryan Conley <ryandeanconley2@yahoo.com>
2020-11-16 11:08:45 -0600

    start of event listeners

---
+++ index.js
@@ -0,0 +1,14 @@
+/* eslint-disable strict */
+function main(){
+  let shoppingList=[];
+
+  $('.add').click(function(){
+    return shoppingList.push('.item');
+  });
+
+  console.log(shoppingList);
+
+}
+
+// eslint-disable-next-line no-undef
+$(main);
